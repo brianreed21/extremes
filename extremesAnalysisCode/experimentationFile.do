82ae847452211e835ad905db370d6ff556144f88
brianreed21 <brian.reed21@gmail.com>
2022-12-30 07:01:47 -0800

    working files for regressions

--- extremesAnalysisCode/experimentationFile.do
+++ extremesAnalysisCode/experimentationFile.do
@@ -1,9 +1,12 @@
-cd ../supplyChain/data/companyData/
+cd ../supplyChain/data/companyData/hqsOnly/
 clear all
 
 
 set maxvar 100000
-import delimited goodsData 
+import delimited goodsData_dirEffects.csv 
+* import delimited goodsData_allSupplierData_dirEffects
+* import delimited goodsData_allCustomerData_indirEffects 
+
 * _withIndDefs
 keep if year != .
 destring gvkey, replace
@@ -14,50 +17,89 @@ describe gvkey
 
 encode yearqtr, generate(time)
 encode indgroup, generate(industry)
-encode indseason, generate(indSeason)
+* encode indseason, generate(indSeason)
 
 keep if ((indgroup!="services") & (indgroup!="finance"))
 
 * encode gsectordesc, generate(industrygics)
 
+
+* different weather variables
+* supplier_extremeheat supplier_heat90plus supplier_extremeprecip
+* extremeprecip heat90plus extremeheat
+* supplier_extremeheat supplier_heat90plus supplier_extremeprecip
+
+
+tempfile results
+local replace replace
+foreach weather of varlist days90_orextreme days90_ifextreme  {
+	display as text %12s "`weather' ***********************************"
+	* revnormd  costnormd opincnormd lnrevnormd_take2 lncostnormd_take2 lnopincnormd_take2
+	foreach outcome of varlist opincnormd revnormd  costnormd  {
+		display as text %12s "`outcome'"
+
+		quietly regress `outcome' c.`weather' i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile, cluster(gvkey)
+		margins, dydx("`weather'") post
+		regsave using "`results'", pval autoid `replace' addlabel(outcome,"`outcome'", weather, "`weather'", hqs_v_estabs, "hqs, no conc reqt", otherNotes, "allCos - nonfin, nonserv") 
+
+		local replace append
+		
+	}
+}
+	
+* Format and outsheet results for use in PivotTable
+use "`results'", clear
+* replace var = subinstr(var,"foreign:","",.)
+outsheet using results_allCos_newVars.csv, replace
+	
 ********************************************************************************
 * this website has the regsave info: # https://julianreif.com/regsave/
 
 
-* tempfile results
-* costnormd_take2 opincnormd revnormd costnormd lnstockclose lnopincnormd lnrevnormd lncostnormd lnopincnormdaf_take2 lnrevnormd_take2 lncostnormd_take2
-* opincnormdaf_take2 opincnormdbef_take2 revnormd_take2 costnormd_take2 lnstockclose lnrevnormd lncostnormd lnopincnormdaf_take2 lnopincnormdbef_take2 lnrevnormd_take2 lncostnormd_take2
-* salesnormd revnormd costnormd othercostnormd totalcostnormd lnsalesnormd_take2 lnothercostnormd_take2 lntotalcostnormd_take2 lncostnormd_take2 lnrevnormd_take2 lnnetincnormd_take2
+*************
+* reg 2 - by background climate
+quietly regress opincnormd c.excessheat90plus##i.temptercile i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile, cluster(gvkey)
+margins, dydx(excessheat90plus) over(temptercilewtd) post
+outreg2 using reg2_hqs.xls, append ctitle("excessheat90plus - controls") label
 
-* local replace replace
-foreach weather of varlist excessrainemp excessheat90plusemp{
-	foreach outcome of varlist salesbyshare salesnormd costbyshare costnormd lnsalesnormd_take2 lncostnormd_take2  lnsalesnormd lncostnormd   {
-	display as text %12s "`outcome'"
-	
-	/* all cos
-	quietly regress `outcome' c.`weather' i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile, cluster(gvkey)
-	margins, dydx("`weather'") post
-	regsave using "`results'", pval autoid `replace' addlabel(outcome,"`outcome'", weather, "`weather'", subsample, "all") 
-	local replace append*/
 
-	/* nonfin cos
-	quietly regress `outcome' c.`weather' i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile if indgroup!="finance", cluster(gvkey)
-	margins, dydx("`weather'") post
-	regsave using "`results'", pval autoid `replace' addlabel(outcome,"`outcome'", weather, "`weather'", subsample, "nonfin")*/ 
-	
-	* nonfin, nonserv cos
-	quietly regress `outcome' c.`weather' i.industry#i.qtr i.time i.gvkey, cluster(gvkey)
-	margins, dydx("`weather'") post
-	* regsave using "`results'", pval autoid `replace' addlabel(outcome,"`outcome'", weather, "`weather'", subsample, "nonfin nonserv") 
-	
-	/*quietly regress `outcome' c.`weather'##i.industrygics i.industrygics#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile if ((indgroup!="services") & (indgroup!="finance")), cluster(gvkey)
-	margins, dydx("`weather'") over(industrygics) post
-	regsave using "`results'", pval autoid `replace' addlabel(outcome,"`outcome'", weather, "`weather'", subsample, "nonfin nonserv by ind") */
-	
+* next rain
+quietly regress opincnormd c.excessrain##i.preciptercile i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile, cluster(gvkey)
+margins, dydx(excessrain) over(preciptercile) post
+outreg2 using reg2_hqs.xls, append ctitle("excessrain - controls") label
+
+
+*************
+* reg 3 - by qtr
+quietly regress opincnormd c.excessheat90plus##i.qtr i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile, cluster(gvkey)
+margins, dydx(excessheat90plus) over(qtr) post
+outreg2 using reg3_hqs.xls, append ctitle("excessheat90plus - controls") label
+
+
+*************
+* reg 5 - by industry
+* next rain
+tempfile results
+local replace replace
+foreach weather of varlist extremeprecip heat90plus extremeheat {
+	display as text %12s "`weather' ***********************************"
+	* revnormd  costnormd opincnormd lnrevnormd_take2 lncostnormd_take2 lnopincnormd_take2
+	foreach outcome of varlist revnormd costnormd opincnormd {
+		display as text %12s "`outcome'"
+		
+		quietly regress `outcome' c.`weather'##i.industry i.industry#i.qtr i.time i.gvkey i.agetercile i.profittercile i.sizetercile, cluster(gvkey)
+		margins, dydx(`weather') over(industry) post
+		
+		regsave using "`results'", pval autoid `replace' addlabel(outcome,"`outcome'", weather, "`weather'", hqs_v_estabs, "hqs by ind", otherNotes, "allCos - nonfin, nonserv") 
+		
+		* outreg2 using reg5_hqs.xls, append ctitle("`outcome' - `weather'") label
+		local replace append
+
 	}
 }
+
 
 * Format and outsheet results for use in PivotTable
 use "`results'", clear
 * replace var = subinstr(var,"foreign:","",.)
-outsheet using results.csv, replace
+outsheet using results_byInd.csv, replace
--- extremesAnalysisCode/prelimRegs.R
+++ extremesAnalysisCode/prelimRegs.R
@@ -9,23 +9,29 @@ library(plm); library(sandwich); library(lmtest); library(fastDummies)
 setwd("~/Documents/supplyChain")
 
 # load: changes data, largest supplier data, and all supplier data
-igData           <- read.csv("data/companyData/igWithWeather.csv") #  %>% select(-X, -opInc_befDep) # before depreciation, things seem a bit more scarce
+igData           <- read.csv("data/companyData/hqsOnly/igWithWeather.csv") #  %>% select(-X, -opInc_befDep) # before depreciation, things seem a bit more scarce
 # igData = igData[igData$locationFracOfEmployees > 0.25,]
 # hist(igData$locationFracOfEmployees, breaks = 50, xlim = c(0,1.0))
 dim(igData)
 
 
-# largestSuppliers <- read.csv("data/companyData/largestSuppliersWithWeather_more500K.csv") %>% select(-X)
 
 
 
-allSuppliers     <- read.csv("data/companyData/allSupplierData.csv")  %>% select(-X)  
+allSuppliers     <- read.csv("data/companyData/hqsOnly/allSupplierData.csv")  %>% select(-X)  
+customerData_largestSupplier <- read.csv("data/companyData/hqsOnly/largestSuppliersWithWeather_more500K.csv") %>% select(-X)
+
+fractions = read.csv('data/companyData/fractionEmployees_byEstablishment.csv') %>% rename(year = archive_version_year,
+                                                                                          gvkey = parent_number) %>% select(-c(latitude, longitude))
 
 
 
 ########################################################################################################################
 # clean the data, first pass 
-data <- allSuppliers 
+data <- igData 
+
+data = merge(data,fractions, how = 'left')
+
 dim(data)
 
 
@@ -37,7 +43,6 @@ dim(data)
 
 ########################################################################################################################
 # run this to get data across all firms
-
 goodsData = data  %>%  mutate(ageTercile    = ntile(earliestYear,3),
          profitTercile = ntile(roa_lagged,3),
          sizeTercile   = ntile(assetsLagged,3),
@@ -45,8 +50,9 @@ goodsData = data  %>%  mutate(ageTercile    = ntile(earliestYear,3),
   
   # this worked very well when it was netIncome/(assetsLast + 0.001), or same for
   # vars other than netIncome
-         revNormd = (totalRevenue + 0.001)/(assetsLast + 0.001),
-         costNormd = (costGoodsSold + 0.001)/(assetsLast + 0.001),
+         revNormd   = (totalRevenue + 0.001)/(assetsLast + 0.001),
+         costNormd  = (costGoodsSold + 0.001)/(assetsLast + 0.001),
+         opIncNormd = (opInc_afDep + 0.001)/(assetsLast + 0.001),
          lnCostNormd          = log(costNormd),
          lnRevNormd           = log(revNormd),
          lnStockClose         = log(priceClose + 1),
@@ -61,8 +67,17 @@ goodsData = data  %>%  mutate(ageTercile    = ntile(earliestYear,3),
          lnNetIncNormd       = Winsorize(lnNetIncNormd, probs = c(0.01, 0.99),na.rm = TRUE),
          lnOpIncNormd        = Winsorize(lnOpIncNormd, probs = c(0.01, 0.99), na.rm = TRUE),
          lnStockClose        = Winsorize(lnStockClose, probs = c(0.01, 0.99), na.rm = TRUE),
-         
-         yearQtr = paste0(year,"_",qtr), firmQtr = paste0(gvkey,'_',qtr), 
+  
+         opIncNormd          = Winsorize(opIncNormd, probs = c(0.025, 0.99),   na.rm = TRUE),
+         revNormd            = Winsorize(revNormd, probs = c(0.01, 0.99),   na.rm = TRUE),
+         costNormd           = Winsorize(costNormd, probs = c(0.01, 0.99),  na.rm = TRUE),
+
+         lnOpIncNormd_take2         = log(opIncNormd + 1),
+         lnCostNormd_take2          = log(costNormd + 1),
+         lnRevNormd_take2           = log(revNormd  + 1),
+  
+  
+         yearQtr = paste0(year,"_",qtr), # firmQtr = paste0(gvkey,'_',qtr), 
          ageQtr  = paste0(ageTercile,"_",yearQtr),
          sizeQtr  = paste0(sizeTercile,"_",yearQtr), 
          profitQtr  = paste0(profitTercile,"_",yearQtr), 
@@ -71,25 +86,40 @@ goodsData = data  %>%  mutate(ageTercile    = ntile(earliestYear,3),
   
   
   # for direct effects
-    mutate(extremeHeat   = temp_zipQuarter95   + lag1_temp_zipQuarter95,
-      heat90Plus    = days90Plus + lag1_days90Plus,
-      streak90Plus  = streak90Plus + lag1_streak90Plus,
-      extremePrecip = precip_zipQuarter95 + lag1_precip_zipQuarter95,
-      tempTercile   = ntile(quarterly_avg_temp,3), 
-      precipTercile = ntile(quarterly_avg_precip,3))
+     mutate(extremeHeat   = temp_zipQuarter_95   + lag1_temp_zipQuarter_95,
+       heat90Plus    = days90Plus + lag1_days90Plus,
+       streak90Plus  = streak90Plus + lag1_streak90Plus,
+       extremePrecipQuarter = precip_zipQuarter95    + lag1_precip_zipQuarter95,
+       extremePrecipAnywhere_days = precip_annual_95 + lag1_precip_annual_95,
+       extremePrecip = precip_zipQuarter_95 + lag1_precip_zipQuarter_95,
+       tempTercile   = ntile(quarterly_avg_temp,3), 
+       precipTercile = ntile(quarterly_avg_precip,3),
+       days_extremeIs90Plus = (days90Plus <= temp_zipQuarter_95)*days90Plus,
+       lag1_days_extremeIs90Plus = (lag1_days90Plus <= lag1_temp_zipQuarter_95)*lag1_days90Plus) %>%
+  rowwise() %>% mutate(days90_Extreme = min(temp_zipQuarter_95, days90Plus),
+                       lag1_days90_Extreme = min(lag1_temp_zipQuarter_95, lag1_days90Plus),
+                       
+                       days90_orExtreme = days90_Extreme + lag1_days90_Extreme,
+                       days90_ifExtreme = days_extremeIs90Plus + lag1_days_extremeIs90Plus)
+
   # firmConcTercile = ntile(locationFracOfEmployees,2)
   # for indirect effects
-   # mutate(supplier_extremeHeat   = supplier_temp_zipQuarter_95   + supplier_lag1_temp_zipQuarter_95,
-   #         supplier_heat90Plus    = supplier_days90Plus + supplier_lag1_days90Plus,
-   #         supplier_streak90Plus  = supplier_streak90Plus + supplier_lag1_streak90Plus,
-   #         supplier_extremePrecip = supplier_precip_zipQuarter_95 + supplier_lag1_precip_zipQuarter_95,
-   #         supplierTempTercile    = ntile(supplier_quarterly_avg_temp,3),
-   #         supplierPrecipTercile  = ntile(supplier_quarterly_avg_precip,3))
+  #   mutate(supplier_extremeHeat   = supplier_temp_zipQuarter_95   + supplier_lag1_temp_zipQuarter_95,
+  #           supplier_heat90Plus    = supplier_days90Plus + supplier_lag1_days90Plus,
+  #           supplier_streak90Plus  = supplier_streak90Plus + supplier_lag1_streak90Plus,
+  #           supplier_extremePrecip = supplier_precip_zipQuarter_95 + supplier_lag1_precip_zipQuarter_95,
+  #           supplierTempTercile    = ntile(supplier_quarterly_avg_temp,3),
+  #           supplierPrecipTercile  = ntile(supplier_quarterly_avg_precip,3)) %>%
+  # rename(gvkey = customer_gvkey)
 
+sum(is.na(goodsData$lnOpIncNormd_take2))
+goodsData = goodsData[complete.cases(goodsData$revNormd) & 
+                        complete.cases(goodsData$costNormd) &
+                        complete.cases(goodsData$opIncNormd), ]
 
-# write.csv(goodsData,"data/companyData/goodsData_largestSupplierData.csv")
-write.csv(goodsData,"data/companyData/goodsData_largestSupplierData_dirEffects.csv")
-
+# write.csv(goodsData,"data/companyData/hqsOnly/goodsData_dirEffects.csv")
+write.csv(goodsData,"data/companyData/hqsOnly/goodsData_allSupplierData_dirEffects.csv")
+# write.csv(goodsData,"data/companyData/hqsOnly/goodsData_allCustomerData_indirEffects.csv")
 
 dim(goodsData)
 
